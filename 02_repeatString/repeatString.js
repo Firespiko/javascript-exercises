e32f1481ebb881f98d8653b15851ba1b2fa4e8fb
K.S.Adithya <adikarks@gmail.com>
2024-05-04 18:05:07 +0530

    Second Exercise(repeat_string) Completed Successfully

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    //Used for throwing error if a negative number is entered
+    if (num < 0){
+        return "ERROR";
+    }
+    
+    repeatedString = "";
+    for(let i = 0;i < num; i++){
+        repeatedString += string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line

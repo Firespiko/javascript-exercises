85b2d91c98d6c08d252b85f8c0757f87476c83e9
K.S.Adithya <adikarks@gmail.com>
2024-05-05 14:09:27 +0530

    Fourth Exercise(removeFromArray) Completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(integerArray, ...removedArray) {
+    const uniqueIntegerSet = new Set();
+    const duplicates = [];
 
+    integerArray.forEach(item => {
+    if (uniqueIntegerSet.has(item)) {
+        duplicates.push(item);
+    } else {
+        uniqueIntegerSet.add(item);
+    }
+    });
+
+    uniqueIntegerArray = Array.from(uniqueIntegerSet);
+
+    for(const element of Array.from(removedArray)) {
+        
+        if (uniqueIntegerArray.indexOf(element) == -1){
+            continue;
+        }
+        
+        uniqueIntegerArray.splice(uniqueIntegerArray.indexOf(element),1);
+    }
+    return uniqueIntegerArray;
 };
 
 // Do not edit below this line

d20686a76486a0ba3a9a8a38568df096907009b0
K.S.Adithya <adikarks@gmail.com>
2024-05-05 13:16:23 +0530

    Fifth Exercise Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(firstNum,secondNum) {
+    
+    if((firstNum < 0) || (secondNum < 0) || 
+    (typeof firstNum != 'number') || (typeof secondNum != 'number')){
+        return "ERROR";
+    } 
 
+    let upperLimit = 0
+    let lowerLimit = 0;
+    let sum = 0;
+    
+    if(firstNum > secondNum) {
+        lowerLimit = secondNum;
+        upperLimit = firstNum;
+    }
+
+    else {
+        lowerLimit = firstNum;
+        upperLimit = secondNum;
+    }
+    
+    for(let num = lowerLimit;num <= upperLimit;num++){
+        sum += num;
+    }
+    return sum;
 };
 
 // Do not edit below this line

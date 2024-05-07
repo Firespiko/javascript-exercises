1d62c19e0ba8360972dd8cadcabe854ff4a3569c
K.S.Adithya <adikarks@gmail.com>
2024-05-07 21:23:34 +0530

    Tenth Exercise(fibonacci) Completed Succesfully

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(term) {
+    if(term == 0){
+        return 0;
+    }
 
+    if(term < 0){
+        return "OOPS";
+    }
+    
+    if((term == 1) || (term == 2)){
+        return 1;
+    }
+    firstTerm = 1;
+    secondTerm = 1;
+    thirdTerm  = firstTerm + secondTerm;
+
+    if(term === 3){
+        return thirdTerm;
+    }
+
+    while((term - 3) > 0){
+        firstTerm = secondTerm;
+        secondTerm = thirdTerm;
+        thirdTerm = firstTerm + secondTerm;
+        term--;
+    }
+
+    return thirdTerm;
 };
 
 // Do not edit below this line

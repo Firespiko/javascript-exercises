29a185280f4bf53a949b17456134dfcb9038c5cb
K.S.Adithya <adikarks@gmail.com>
2024-05-07 21:09:59 +0530

    8th Exercise Completed(Calculator)

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a , b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a , b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+
+	return  arr.reduce((total,currentItem) =>{
+    return total + currentItem;
+  },0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total,currentItem) =>{
+    return total * currentItem;
+  },1);
 };
 
-const power = function() {
-	
+const power = function(a , b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if((a == 0) || (a == 1)){
+    return 1;
+  }
+  
+  let factorialValue = 1;
+  for(let num = 1; num <= a; num++){
+    factorialValue *= num;
+  }
+  return factorialValue
 };
 
 // Do not edit below this line
